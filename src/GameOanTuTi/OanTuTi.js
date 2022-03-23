d7a29aa7624f642f14baa8d7b8fca382224de9c2
Nam <phamhainamkt@gmail.com>
2022-03-23 14:01:20 +0700

    thêm link deloy

--- src/GameOanTuTi/OanTuTi.js
+++ src/GameOanTuTi/OanTuTi.js
@@ -3,11 +3,13 @@ import DatCuoc from './DatCuoc'
 import KetQua from './KetQua'
 import '../GameOanTuTi/OanTuTi.css'
 
+// Link Deloy: https://bcdn-05-baitap-redux-pham-hai-nam.vercel.app/
+//             https://bcdn-05-baitap-redux-pham-hai-nam-git-master-hainamkt.vercel.app/
+
 export default class OanTuTi extends Component {
   render() {
     return (
       <div className='container-fluid text-center GameOanTuTi'>
-          {/* <h1>Phạm Hải Nam</h1> */}
           <DatCuoc/>
           <KetQua/>
       </div>
